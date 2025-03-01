c846c7d7e4c96c225b4cd63d752194860342b371
Fahim <faahim06@gmail.com>
2025-03-01 08:09:22 +0600

    refactor email sending logic to update sender information and improve code formatting

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -3,33 +3,33 @@ import { Client as QStashClient, resend } from "@upstash/qstash";
 import config from "@/lib/config";
 
 export const workflowClient = new WorkflowClient({
-  baseUrl: config.env.upstash.qstashUrl,
-  token: config.env.upstash.qstashToken,
+    baseUrl: config.env.upstash.qstashUrl,
+    token: config.env.upstash.qstashToken,
 });
 
 const qstashClient = new QStashClient({
-  token: config.env.upstash.qstashToken,
+    token: config.env.upstash.qstashToken,
 });
 
 export const sendEmail = async ({
-  email,
-  subject,
-  message,
+    email,
+    subject,
+    message,
 }: {
-  email: string;
-  subject: string;
-  message: string;
+    email: string;
+    subject: string;
+    message: string;
 }) => {
-  await qstashClient.publishJSON({
-    api: {
-      name: "email",
-      provider: resend({ token: config.env.resendToken }),
-    },
-    body: {
-      from: "JS Mastery <contact@adrianjsmastery.com>",
-      to: [email],
-      subject,
-      html: message,
-    },
-  });
+    await qstashClient.publishJSON({
+        api: {
+            name: "email",
+            provider: resend({ token: config.env.resendToken }),
+        },
+        body: {
+            from: "Fahim <hello.fahimbuilds.me>",
+            to: [email],
+            subject,
+            html: message,
+        },
+    });
 };
